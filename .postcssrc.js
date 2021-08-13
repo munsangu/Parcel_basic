fa536fcc576799d1cbfdd7db584210201f775dee
munsangu <ds-3632@hanmail.net>
2021-08-14 00:09:19 +0900

    20210814

---
+++ .postcssrc.js
@@ -0,0 +1,25 @@
+// ESM
+// CommonJs
+
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+// // export{
+// //     plugins: [
+// //       autoprefixer
+// //     ]
+// // }
+
+// module.exports = {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+
+// 간소화
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
